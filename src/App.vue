c39aa3ffdb6995e5a50d954dfd2c5e324fd22a17
leospurs <lehojae@gmail.com>
2022-06-21 17:11:06 +0900

    vue.js

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1 @click="increase">{{ count }}</h1>
+    <div v-if="count > 4">4보다 큽니다.</div>
+    <ul>
+      <Fruit v-for="fruit in fruits" :key="fruit" :name="fruit">
+        {{ fruit }}
+      </Fruit>
+    </ul>
+</template>
+
+<script>
+import Fruit from '~/components/Fruits'
+export default {
+  components : {
+    Fruit
+  },
+    data() {
+      return {
+        count: 0,
+        fruits: ['Apple', 'Banana', "cherry"]
+      }  
+    },
+    methods: {
+      increase() {
+        this.count += 1
+      }
+    }
+}
+</script>
+
+<style lang="scss">
+ 
+</style>
